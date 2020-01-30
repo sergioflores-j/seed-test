9cfbd55402516aa745480b10a7b89a14b3fd20b8
Sergio Eduardo Flores Junior <sergio@adapcon.com.br>
2020-01-30 07:45:00 -0300

    apply code changes on users ms to trigger the deploy

--- services/users/handler.js
+++ services/users/handler.js
@@ -3,16 +3,11 @@
 module.exports.hello = async event => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify({
+      users: [
+        { id: 1, name: 'João' },
+        { id: 2, name: 'Maria' },
+      ],
+    })
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
